0642887ac413f1b44d677fd7a81efc3677865108
Ludovico Gerardi <ludovico.gerardi@posteo.it>
2025-12-28 10:56:12 +0100

    Aggiungi struttura dati per mantenere i binding

--- PseudoXML/PseudoXMLParserSupport.c
+++ PseudoXML/PseudoXMLParserSupport.c
@@ -62,3 +62,90 @@ int check_sublevel_tag_order(SourceFile p) {
   }
   return 0;
 }
+
+section_entry* create_section_entry(char* name, section_entry* next) {
+  section_entry* myself = (section_entry*) malloc(sizeof(*myself));
+  myself->name = name;
+  myself->next = next;
+  return myself;
+}
+
+field_entry* create_field_entry(char* name, section_entry* section, field_entry* next) {
+  field_entry* myself = (field_entry*) malloc(sizeof(*myself));
+
+  myself->field_name = name;
+  myself->section = section;
+  myself->next = next;
+  
+  return myself;
+}
+
+field_entry* create_field_entry_Integer(char* name, section_entry* section, field_entry* next, Integer value) {
+  field_entry* myself = create_field_entry(name, section, next);
+  myself->kind = is_Integer;
+  myself->value_Integer = value;
+  return myself;
+}
+
+field_entry* create_field_entry_Bool(char* name, section_entry* section, field_entry* next, int value) {
+  field_entry* myself = create_field_entry(name, section, next);
+  myself->kind = is_Boolean;
+  myself->value_Boolean = value;
+  return myself;
+}
+
+field_entry* create_field_entry_String(char* name, section_entry* section, field_entry* next, String value) {
+  field_entry* myself = create_field_entry(name, section, next);
+  myself->kind = is_String;
+  myself->value_String = value;
+  return myself;
+}
+
+field_entry* create_field_entry_Local(char* name, section_entry* section, field_entry* next, field_entry* copy_from) {
+  field_entry* myself = create_field_entry(name, section, next);
+  myself->kind = is_Local;
+  myself->copy_from = copy_from;
+  copy_from->backlinks = create_backlink(myself, copy_from->backlinks);
+  return myself;
+}
+
+field_entry* create_field_entry_NonLocal(char* name, section_entry* section, field_entry* next, field_entry* copy_from) {
+  field_entry* myself = create_field_entry(name, section, next);
+  myself->kind = is_NonLocal;
+  myself->copy_from = copy_from;
+  return myself;
+}
+
+field_entry* create_field_entry_Inherited(char* name, section_entry* section, field_entry* next, field_entry* inherit_from) {
+  field_entry* myself = create_field_entry(name, section, next);
+  myself->kind = is_Inherited;
+  myself->inherit_from = inherit_from;
+  return myself;
+}
+
+backlink* create_backlink(field_entry* ptr, backlink* next) {
+  backlink* myself = (backlink*) malloc(sizeof(*myself));
+  myself->ptr = ptr;
+  myself->next = next;
+  return myself;
+}
+
+/* section_entry* create_bindings_from_tree(SourceFile tree) {
+  section_entry* bindings = NULL;  
+  ListTopLevelTag tl = tree->u.mainFile_.listtopleveltag_; 
+  TopLevelTag t;
+  field_entry* f;
+  while (tl) {
+    t = tl->topleveltag_;
+    if (t->kind == is_FileImportTag) {
+      continue;
+    }
+
+    char* section_name = t->u.sectionTag_.ident_;
+    bindings = create_section_entry(section_name, bindings);
+
+    sl
+    
+  }
+  return bindings;
+} */
--- PseudoXML/PseudoXMLParserSupport.h
+++ PseudoXML/PseudoXMLParserSupport.h
@@ -11,4 +11,56 @@ int check_toplevel_tag_order(SourceFile p);
 di tutti i field; se è così ritorna 0, altrimenti ritorna 1. */
 int check_sublevel_tag_order(SourceFile p);
 
+/* strutture dati per mantenere i binding */
+typedef struct section_entry section_entry;
+typedef struct field_entry field_entry;
+typedef struct backlink backlink;
+
+/* lista concatenata di sezioni */
+struct section_entry {
+  char* name;
+  section_entry* prev;
+  section_entry* next;
+  field_entry* fields;
+};
+
+section_entry* create_section_entry(char* name, section_entry* next);
+
+/* lista concatenata di campi */
+struct field_entry {
+  char* field_name;
+  enum { is_Integer, is_Boolean, is_String, is_Local, is_NonLocal, is_Inherited } kind;
+  union {
+    Integer value_Integer;
+    int value_Boolean;
+    String value_String;
+  };
+  section_entry* section;
+  field_entry* prev;
+  field_entry* next;
+  field_entry* inherit_from;
+  field_entry* copy_from;
+  backlink* backlinks;
+};
+
+field_entry* create_field_entry(char* name, section_entry* section, field_entry* next);
+field_entry* create_field_entry_Integer(char* name, section_entry* section, field_entry* next, Integer value);
+field_entry* create_field_entry_Bool(char* name, section_entry* section, field_entry* next, int value);
+field_entry* create_field_entry_String(char* name, section_entry* section, field_entry* next, String value);
+field_entry* create_field_entry_Local(char* name, section_entry* section, field_entry* next, field_entry* copy_from);
+field_entry* create_field_entry_NonLocal(char* name, section_entry* section, field_entry* next, field_entry* copy_from);
+field_entry* create_field_entry_Inherited(char* name, section_entry* section, field_entry* next, field_entry* inherit_from);
+
+/* lista concatenata di riferimenti a campi */
+struct backlink {
+ field_entry* ptr;
+ backlink* next;
+};
+
+backlink* create_backlink(field_entry* ptr, backlink* next);
+
+
+/* dato un albero, popola la struttura dati con i binding */
+section_entry* create_bindings_from_tree(SourceFile tree);
+
 #endif
