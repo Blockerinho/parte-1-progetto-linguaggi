b801de9d750bee58726b77fd7ef5a9a5d9834923
Aleksa Aleksic <124301560+Blockerinho@users.noreply.github.com>
2026-01-04 15:02:54 +0100

    Update ParserSupport.h

--- ParserSupport.h
+++ ParserSupport.h
@@ -1,7 +1,6 @@
 #ifndef PARSUP_H
 #define PARSUP_H
 
-
 #ifdef DETECT_IMPORT_CYCLES
 #include <sys/types.h>
 #endif
@@ -19,10 +18,11 @@ struct section_entry {
   section_entry* prev;
   section_entry* next;
   field_entry* fields;
+  int line; /* Aggiunto dal secondo file */
 };
 
 /* Crea una sezione vuota dato il nome e il prossimo elemento della lista. */
-section_entry* create_section_entry(char* name, section_entry* next);
+section_entry* create_section_entry(char* name, section_entry* next, int line);
 
 /* Rappresenta un campo. I puntatori prev e next permettono di formare una
    lista concatenata. Il puntatore references punta al campo a cui questo
@@ -38,6 +38,7 @@ struct field_entry {
     int value_Boolean;
     char* value_String;
   };
+  int line; /* Aggiunto dal secondo file */
   field_entry* prev;
   field_entry* next;
   field_entry* references;
@@ -47,11 +48,11 @@ struct field_entry {
 /* Crea un campo dato nome, valore, sezione di appartenenza, lista degli altri
    campi, e bindings creati fino a quel momento (da usare nel caso di campi
    che si riferiscono ad altri campi in altre sezioni). */
-field_entry* create_field_entry(char* name, Value value, section_entry* section, field_entry* next, section_entry* bindings);
+field_entry* create_field_entry(char* name, Value value, section_entry* section, field_entry* next, section_entry* bindings, int line);
 
 /* Crea un campo ereditato dato nome, sezione di appartenenza, campo da cui
    eredita. */
-field_entry* create_field_entry_inherited(char* name, section_entry* section, field_entry* inherited_from);
+field_entry* create_field_entry_inherited(char* name, section_entry* section, field_entry* inherited_from, int line);
 
 /* Dato il nome di una sezione, la sezione corrente e i bindings, eredita
    tutti i campi della sezione con quel nome nella sezione corrente. */
@@ -90,8 +91,6 @@ field_entry* search_bindings_local(char* field_name, field_entry* fields);
 field_entry* search_bindings_nonlocal(section_entry* bindings, char* section_name, char* field_name);
 
 
-
-
 /* risoluzione dei valori */ 
 field_entry* resolve_field(field_entry* field);
 Value resolve_value(field_entry* field);
@@ -119,4 +118,5 @@ void free_all_sections(section_entry* sections);
    Tra { } c'è la lista dei backlink.
  */
 void print_bindings(section_entry* bindings);
+
 #endif
